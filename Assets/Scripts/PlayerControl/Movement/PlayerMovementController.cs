f5c945e0a2494d5f45ffcf8de213a3720cded510
Hakan Ayhan <hakanayhnn@gmail.com>
2023-02-19 19:25:23 +0300

    +adjust: rotation controller for turn

--- Assets/Scripts/PlayerControl/Movement/PlayerMovementController.cs
+++ Assets/Scripts/PlayerControl/Movement/PlayerMovementController.cs
@@ -9,11 +9,12 @@ namespace TopDownShooter.PlayerControls
     {
         [SerializeField] private InputData _inputData;
         [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Transform _targetTransform;
         [SerializeField] private PlayerMovementSettings _playerMovementSettings;
         private void Update()
         {
             _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.forward * _inputData.Vertical * _playerMovementSettings.VerticalSpeed));
-            _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.right * _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed));
+            _targetTransform.Rotate(0, _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed, 0, Space.Self);
         }
     }
 }
